28303632c430c31cef18b27215f21165b1137c9b
Kath Nicholls <nichollskc@gmail.com>
2020-12-19 15:41:58 +0000

    Now working with key to match lists across browsers

--- random.js
+++ random.js
@@ -4,6 +4,8 @@ $(function(){
     populateSelect('#verbs', lists.verbs);
     populateSelect('#subjects', lists.subjects);
 
+    populateKeyOptions('harrypotter');
+
     // Open list chooser
     $('.js-chooseLists').click(function(e){
         if(!($('.js-wrapper--listChooser').hasClass('open'))){
@@ -38,15 +40,65 @@ function randomise(list, rowID, seedKey) {
 
 function randomiseWords() {
     var list = lists['codenames'].slice();
-    console.log(list);
-    var seedKey = document.getElementById('teamKey').value;
-    randomise(list, 'randomWords', seedKey);
+    var today = new Date().toLocaleDateString();
+    var seedKey = document.getElementById('selectTeamKey').value;
+    var fullSeedKey = seedKey + ' ' + today;
+    console.log(fullSeedKey);
+    Math.seedrandom(fullSeedKey);
+    var randomValue = Math.random();
+    console.log(randomValue);
+
+    var check = document.getElementById('keyCheck');
+    check.innerHTML = Math.trunc(randomValue * 10000);
+    randomise(list, 'randomWords', fullSeedKey);
 }
 
 function randomiseNumbers() {
     var list = [1,2,3,4];
-    console.log(list);
     randomise(list, 'randomNumbers', null);
+}
+
+function toggleRow(rowID, buttonID, showText, hideText) {
+    var button = document.getElementById(buttonID);
+    console.log(button.value);
+    if (button.value == showText) {
+        button.value = hideText;
+        console.log("Showing");
+        $(rowID).css('visibility', 'visible');
+    } else {
+        button.value = showText;
+        console.log("Hiding");
+        $(rowID).css('visibility', 'hidden');
+    }
+}
+
+function toggleWords() {
+    toggleRow('#randomWords', 'toggleWordsButton', 'Show words', 'Hide words');
+}
+
+function toggleNumbers() {
+    toggleRow('#randomNumbers', 'toggleNumbersButton', 'Show code', 'Hide code');
+}
+
+
+function populateKeyOptions(listName) {
+    var keyOptionsEl = document.getElementById("teamKeyList");
+    var options = lists[listName];
+    console.log(options);
+
+    // Clear existing options
+    while (keyOptionsEl.firstChild) {
+        keyOptionsEl.firstChild.remove()
+    }
+
+    // Add all options from the list
+    for(var i = 0; i < options.length; i++) {
+        var opt = options[i];
+        var el = document.createElement("option");
+        el.textContent = opt;
+        el.value = opt;
+        keyOptionsEl.appendChild(el);
+    }
 }
 
 /* Helper functions
